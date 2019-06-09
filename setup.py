c2e21692edfb4a0c99b5c65991fdf2e8e8c62bc9
Karl N. Redman <karl.redman@gmail.com>
2019-06-09 03:16:32 -0500

    testing environment

--- setup.py
+++ setup.py
@@ -1,11 +1,34 @@
-from setuptools import setup
+import setuptools
 
-setup(name='editfrontmatter',
-      version='0.0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
-      packages=['editfrontmatter'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='editfrontmatter',
+    version='0.0.1',
+    description='Edit front matter with Jinja2 templates',
+    project_urls={
+        "Documentation": "http://github.com/karlredman/EditFrontMatter",
+        "Source Code": "https://karlredman.github.io/EditFrontMatter/"
+    },
+    author='Karl N. Redman',
+    author_email='karl.redman@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='jinja2 markdown front-matter',
+    packages=setuptools.find_packages(
+        exclude=['docs', 'docsource']
+    ),
+    install_requires=[
+        "Jinja2",
+        "MarkupSafe",
+        "oyaml",
+        "PyYAML",
+    ],
+    python_requires='>3.6.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ])
+
