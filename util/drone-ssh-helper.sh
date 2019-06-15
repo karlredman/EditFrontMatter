b1422377e0f94d9c53fe766aa49ba1baf4618df9
Karl N. Redman <karl.redman@gmail.com>
2019-06-15 08:23:42 -0500

    wip builds: testing deploy: success?

--- util/drone-ssh-helper.sh
+++ util/drone-ssh-helper.sh
@@ -3,6 +3,10 @@
 # reference:  [[SOLVED] Can i use plugins/git to clone another repo? - General Discussion - Drone](https://discourse.drone.io/t/solved-can-i-use-plugins-git-to-clone-another-repo/1553/6?u=karlredman)
 # reference: [Secret in Drone 1.0.0-rc.1 · Issue #130 · appleboy/drone-ssh](https://github.com/appleboy/drone-ssh/issues/130)
 
+# the trick to making this work is to add the ssh_key from the drone cli:
+DRONE_SERVER=$DRONE_SERVER DRONE_TOKEN=$DRONE_TOKEN sudo -E bash -c 'drone secret add --repository ${SSH_USER}/${CI_REPO} --name ssh_key --data @.ssh/id_rsa'
+
+
 # Configures ssh key based on information from secrets
 
 # only execute the script when github token exists.
@@ -20,7 +24,7 @@ ssh-keyscan -H $SSH_HOST > /etc/ssh/ssh_known_hosts 2> /dev/null
 
 # reset repo origin
 git remote remove origin
-git remote add origin git@github.com:karlredman/EditFrontMatter.git
+git remote add origin git@${SSH_HOST}:${LOGIN_NAME}/${CI_REPO}.git
 git remote -v
 # push to repo
-git push git@github.com:karlredman/EditFrontMatter.git master
+git push git@$SSH_HOST:${LOGIN_NAME}/${CI_REPO}.git ${BRANCH}
