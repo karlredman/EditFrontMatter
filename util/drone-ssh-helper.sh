3df4fe1639d0e1096d558af9d322e046e9c6f2d9
Karl N. Redman <karl.redman@gmail.com>
2019-06-15 06:40:25 -0500

    wip builds: testing deply

--- util/drone-ssh-helper.sh
+++ util/drone-ssh-helper.sh
@@ -18,8 +18,9 @@ touch /root/.ssh/known_hosts
 chmod 600 /root/.ssh/known_hosts
 ssh-keyscan -H $SSH_HOST > /etc/ssh/ssh_known_hosts 2> /dev/null
 
-# push to repo
+# reset repo origin
 git remote remove origin
 git remote add origin git@github.com:karlredman/EditFrontMatter.git
 git remote -v
+# push to repo
 git push origin master
